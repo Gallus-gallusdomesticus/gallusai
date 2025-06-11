3f91677952afa69f835968107705fdf7edb858d9
Gallus-gallusdomesticus <stephenefelson1999@gmail.com>
2025-06-11 20:37:38 +1000

    Change the main.py to accept various prompt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+import sys
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -8,9 +9,14 @@ from google import genai
 
 client = genai.Client(api_key=api_key)
 
-cont=client.models.generate_content(model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
 
-print(cont.text)
+
+if len(sys.argv)==1:
+    print("Error, no contents provided!")
+    sys.exit(1)
+else:
+    cont=client.models.generate_content(model="gemini-2.0-flash-001", contents=" ".join(sys.argv[1:]))
+    print(cont.text)
 x=cont.usage_metadata.prompt_token_count
 y=cont.usage_metadata.candidates_token_count
 print(f"Prompt tokens: {x}")
