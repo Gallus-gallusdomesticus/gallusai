4aed378b9193672f1d784ab3bba1cb0e3f8e19be
Gallus-gallusdomesticus <stephenefelson1999@gmail.com>
2025-06-19 06:42:07 +1000

    Add call_function and prompts

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import os
-from dotenv import load_dotenv
 import sys
+
 from google import genai
 from google.genai import types
+from dotenv import load_dotenv
+
+from prompts import system_prompt
+from call_function import available_functions
 
 def main():
     load_dotenv()
+
+    verbose="--verbose" in sys.argv
 
     args=[arg for arg in sys.argv[1:] if arg!="--verbose"]
     if not args:
@@ -19,22 +25,33 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
+    if verbose:
+        print(f"User prompt: {user_prompt}\n")
+
     messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
 
-    generate_content(client, messages)
+    generate_content(client, messages, verbose)
 
-def generate_content(client, messages):
-    response=client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
 
-    if "--verbose" in sys.argv:
-        print("User prompt:")
-        print(response.text)
+
+def generate_content(client, messages, verbose):
+
+    response=client.models.generate_content(model="gemini-2.0-flash-001", contents=messages, 
+    config=types.GenerateContentConfig(
+        tools=[available_functions], system_instruction=system_prompt))
+
+
+    if verbose:
         x=response.usage_metadata.prompt_token_count
         y=response.usage_metadata.candidates_token_count
         print(f"Prompt tokens: {x}")
         print(f"Response tokens: {y}")
-    else:
-        print(response.text)
+
+    if not response.function_calls:
+        return response.text
+
+    for function_call_part in response.function_calls:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
 
     
 
